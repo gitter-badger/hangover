cdeba9f011aa0401eb57d46895c1bbdeee149dfe
Paul Rad <p.rad@dispofi.fr>
2014-10-27 15:46:51 +0100

    improve angularjs example

--- assets/js/build/all.js
+++ assets/js/build/all.js
@@ -281,23 +281,67 @@ this.current.$$route){var c={},f=this;e.forEach(Object.keys(a),function(b){f.cur
 
     var App = $angular.module('App', ['ngResource']);
 
+    App.factory('resourceInterceptor', ['$rootScope', function($rootScope) {
+        $rootScope.http = {};
+        return {
+            request: function() {
+                $rootScope.http.load = true;
+            },
+            response: function() {
+                $rootScope.http.load = false;
+                $rootScope.http.success = true;
+            },
+            responseError: function(e) {
+                $rootScope.http.load = false;
+                $rootScope.http.success = false;
+                $rootScope.http.error = e;
+            }
+        };
+    }])
 
-    App.factory('Invitations', ['$resource', function($resource) {
-        return $resource('/invitations/:id', {id: '@id'});
+    App.factory('Invitations', ['$resource', 'resourceInterceptor', function($resource, resourceInterceptor) {
+        return $resource('/invitations/:id', {id: '@id'}, {
+            save: {
+                method: 'POST',
+                interceptor: resourceInterceptor
+            }
+        });
     }]);
 
+    App.directive('httpcallback', ['$rootScope', function($rootScope) {
+        return {
+            restrict: 'EA',
+            template: [
+                '<div>',
+                    '<div ng-show="state.load">Chargement en cours</div>',
+                    '<div ng-show="!state.load">',
+                        '<div ng-show="state.success">Insertion reussie</div>',
+                        '<div ng-show="state.success === false">',
+                            '<div>Une erreur est survenue, détails ci-dessous</div>',
+                            '<pre>{{state.error|json}}</pre>',
+                        '</div>',
+                    '</div>',
+                '</div>'
+            ].join(''),
+            link: function($scope) {
+                $scope.state = $rootScope.http;
+            }
+        }
+    }]);
 
     App.controller('RootController', ['$scope', 'Invitations', function($scope, Invitations) {
 
+        $scope.httpResponse = null;
         $scope.email = '';
 
         $scope.submission = (function SubmitEmail() {
             var invitation = new Invitations({
-                email: 'test'
+                email: $scope.email
             });
-            console.log(invitation)
+
             invitation.$save(function(data) {
-                console.log(data);
+                console.log(invitation);
+                $scope.httpResponse = data;
             });
         });
     }]);
