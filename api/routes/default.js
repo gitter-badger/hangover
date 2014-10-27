a988e98196a3ca0cda3f7a5ebf7007a88e3561dd
Paul Rad <p.rad@dispofi.fr>
2014-10-27 09:25:00 +0100

    add default route for static files

--- api/routes/default.js
+++ api/routes/default.js
@@ -6,6 +6,16 @@ module.exports = [
         method: 'GET',
         path: '/',
         handler: Ho.controller('default')
+    },
+    
+    {
+        method: 'GET',
+        path: '/assets/{param*}',
+        handler: {
+            directory: {
+                path: require('path').join(__dirname, '../../assets/')
+            }
+        }
     }
 
 ];
